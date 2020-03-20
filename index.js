8ced5bd347400a3d0b0641040cbc79a99e8c9867
Cesar GP <cgpcsjcmp@gmail.com>
2020-03-20 13:17:33 +0100

    ADD: Minimal config + coverage

---
+++ index.js
@@ -0,0 +1,9 @@
+const addTwoNumbers = (a, b) => {
+  return a + b
+}
+const addThreeNumbers = (a, b, c) => {
+  return a + b + c
+}
+
+exports.addTwoNumbers = addTwoNumbers
+exports.addThreeNumbers = addThreeNumbers
